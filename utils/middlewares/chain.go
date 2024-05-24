8b2f47c1990d0fa3d978e76864f9a3c119a4d697
Roham Hayedi <rohamhayedi@gmail.com>
2024-05-24 19:12:36 +0330

    rm debug log

--- utils/middlewares/chain.go
+++ utils/middlewares/chain.go
@@ -1,35 +1,19 @@
 package middlewares
 
 import (
-	// "fmt"
-	"reflect"
-	"runtime"
 	"slices"
 
 	"github.com/rohhamh/go-shopping-cart-crud/middlewares"
 )
 
-func GetFunctionName(i interface{}) string {
-    return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
-}
-
 func Chain (middlewaresChain *[]middlewares.Middleware, handler *middlewares.RequestHandler) middlewares.RequestHandler {
     if middlewaresChain == nil { return *handler }
 
     slices.Reverse(*middlewaresChain)
-    middlewaresHandlers := []middlewares.RequestHandler {*handler}
-    finalHandler := handler
     for _, middleware := range *middlewaresChain {
-        middlewareHandler := middleware(finalHandler)
-        finalHandler = &middlewareHandler
-        middlewaresHandlers = append(middlewaresHandlers, *finalHandler)
+        middlewareHandler := middleware(handler)
+        handler = &middlewareHandler
     }
     slices.Reverse(*middlewaresChain)
-
-    // // fmt.Printf("handlers for %s: with chain %v\n", GetFunctionName(*handler), *middlewaresChain)
-    // for _, mid := range middlewaresHandlers {
-    //     fmt.Printf("\t%v %s\n", mid, GetFunctionName(mid))
-    // }
-    // fmt.Println()
-    return *finalHandler
+    return *handler
 }
